01e1c672852e051488dd701cca848e3f74799430
Bihan Viranga <bihanviranga@gmail.com>
2018-07-28 09:48:30 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,9 +5,21 @@ require 'pry'
 def create_project_hash
     html = File.read("fixtures/kickstarter.html")
     kickstarter = Nokogiri::HTML(html)
-    binding.pry
+
+    projects = {}
+    
+    
 end
 
 
-create_project_hash
+
+# projects = kickstarter.css("li.project.grid_4")
+# title = project.css("h2.bbcard_name strong a").text
+# image link = project.css("div.project-thumbnail a img").attribute("src").value
+# description = project.css("p.bbcard_blurb").text
+# by = project.css("h2.bbcard_name span").text 
+# location = project.css("ul.project-meta li a").text.strip
+# funded = project.css("ul.project-stats .first.funded").text.strip.gsub("\n"," ")
+# pledged = project.css("ul.project-stats li.pledged").text.strip.gsub("\n"," ")
+
 
